eae5d9f399c6325c323a5cadc6c20148e9d10d83
NEPTR33 <baiolemi@gmail.com>
2024-02-07 22:42:44 -0500

    added chstours

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,20 +13,20 @@ function Body() {
         title="CHSTours"
         img="CHSTours.webp"
         body="CHS Tours is a full stack application that utilizes pug for server side rendering. I used MongoDB along with node/express, and bridged everything together with a REST API. The app has full functioning user signup/login capibilities, and all of the tours/information come dynamically from the database."
-        techStack={["Node.js/Express", "Pug", "RESTful API", ""]}
+        techStack={["Node.js/Express", "Pug", "RESTful API", "JavaScript"]}
         link="https://lit-temple-20750-d72cb4651e0d.herokuapp.com/"
       />
       <WebCard
         title="Exotico"
         img="ExotiCo-homepage.webp"
-        body="ExotiCo is a web page that I built for a local reptile education company. They bring reptiles to birthday parties and schools to teach kids about nature preservation! "
+        body="ExotiCo is a web page that I built for a local reptile education company. I created this site using React along with Material UI for simple and clean design. This company brings reptiles to birthday parties and schools to teach kids about nature preservation! "
         techStack={["React.js", "MUI", "React-Router", "Formspree"]}
         link="https://exoticocharleston.com"
       />
       <WebCard
         title="ScoreScore"
         img="scorescore.png"
-        body="ScoreScore was my summer internship project with Code and Trust. This experience has taught me what it’s like to work with a team of developers, and how to overcome many challenges and tight deadlines. "
+        body="ScoreScore was my summer internship project with Code and Trust. This full stack application uses mySQL and node.js on the backend, and React/MUI on the frontend. This experience has taught me what it’s like to work with a team of developers, and how to overcome many challenges and tight deadlines. "
         techStack={
           isMobile
             ? ["React.ts", "MUI", "React-Query", "mySQL"]
@@ -37,16 +37,9 @@ function Body() {
       <WebCard
         title="movieSnacks"
         img="moviesnacks.png"
-        body="movieSnacks is a react app that fetches movie data from the omd api and renders a list of movies you can select, rate, and add to your watch list. "
+        body="movieSnacks is a react app that fetches movie data from the Open Movie Database API, and renders a list of movies you can select, rate, and add to your watch list. This was one of my first React projects. and realy helped me learn the fundementals within the React ecosystem."
         techStack={["React.js", "HTML", "CSS"]}
         link="https://boingloings33.github.io/movieSnacksApp/"
-      />
-      <WebCard
-        title="baioCharts"
-        img="baiocharts.png"
-        body="My very first personal project. It is a simple javascript/html/css application that fetches stock data from a 3rd party api, and uses a library (Chart.js) to display the data. "
-        techStack={["JavaScript", "HTML", "CSS", "Chart.js"]}
-        link="https://baiocharts.com/"
       />
     </Stack>
   );
